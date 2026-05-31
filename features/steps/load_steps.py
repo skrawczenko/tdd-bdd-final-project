ab8a43e24d553d10eaec0a58bce5afd8fa63bb94
skrawczenko <skrawczenko@gmail.com>
2026-05-31 04:17:38 -0400

    features2

--- features/steps/load_steps.py
+++ features/steps/load_steps.py
@@ -55,4 +55,5 @@ def step_impl(context):
             "category": row['category']
         }
         context.resp = requests.post(rest_endpoint, json=payload)
-        assert context.resp.status_code == HTTP_201_CREATED%  
+        assert context.resp.status_code == HTTP_201_CREATED
+
